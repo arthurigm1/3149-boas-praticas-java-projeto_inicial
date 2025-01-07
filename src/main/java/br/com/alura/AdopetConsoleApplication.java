0ce9f224aae774ab43b945949a9ffb6cd240ce02
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-01-07 12:26:49 -0300

    updateif

--- src/main/java/br/com/alura/AdopetConsoleApplication.java
+++ src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -31,6 +31,8 @@ public class AdopetConsoleApplication {
                 String textoDigitado = new Scanner(System.in).nextLine();
                 opcaoEscolhida = Integer.parseInt(textoDigitado);
 
+
+
                 if (opcaoEscolhida == 1) {
                     HttpClient client = HttpClient.newHttpClient();
                     String uri = "http://localhost:8080/abrigos";
@@ -49,36 +51,8 @@ public class AdopetConsoleApplication {
                         System.out.println(id +" - " +nome);
                     }
                 } else if (opcaoEscolhida == 2) {
-                    System.out.println("Digite o nome do abrigo:");
-                    String nome = new Scanner(System.in).nextLine();
-                    System.out.println("Digite o telefone do abrigo:");
-                    String telefone = new Scanner(System.in).nextLine();
-                    System.out.println("Digite o email do abrigo:");
-                    String email = new Scanner(System.in).nextLine();
+                    CadastrarAbrigo();
 
-                    JsonObject json = new JsonObject();
-                    json.addProperty("nome", nome);
-                    json.addProperty("telefone", telefone);
-                    json.addProperty("email", email);
-
-                    HttpClient client = HttpClient.newHttpClient();
-                    String uri = "http://localhost:8080/abrigos";
-                    HttpRequest request = HttpRequest.newBuilder()
-                            .uri(URI.create(uri))
-                            .header("Content-Type", "application/json")
-                            .method("POST", HttpRequest.BodyPublishers.ofString(json.toString()))
-                            .build();
-
-                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                    int statusCode = response.statusCode();
-                    String responseBody = response.body();
-                    if (statusCode == 200) {
-                        System.out.println("Abrigo cadastrado com sucesso!");
-                        System.out.println(responseBody);
-                    } else if (statusCode == 400 || statusCode == 500) {
-                        System.out.println("Erro ao cadastrar o abrigo:");
-                        System.out.println(responseBody);
-                    }
                 } else if (opcaoEscolhida == 3) {
                     System.out.println("Digite o id ou nome do abrigo:");
                     String idOuNome = new Scanner(System.in).nextLine();
@@ -172,6 +146,38 @@ public class AdopetConsoleApplication {
             System.out.println("Finalizando o programa...");
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+    private static void CadastrarAbrigo() throws IOException, InterruptedException {
+        System.out.println("Digite o nome do abrigo:");
+        String nome = new Scanner(System.in).nextLine();
+        System.out.println("Digite o telefone do abrigo:");
+        String telefone = new Scanner(System.in).nextLine();
+        System.out.println("Digite o email do abrigo:");
+        String email = new Scanner(System.in).nextLine();
+
+        JsonObject json = new JsonObject();
+        json.addProperty("nome", nome);
+        json.addProperty("telefone", telefone);
+        json.addProperty("email", email);
+
+        HttpClient client = HttpClient.newHttpClient();
+        String uri = "http://localhost:8080/abrigos";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .header("Content-Type", "application/json")
+                .method("POST", HttpRequest.BodyPublishers.ofString(json.toString()))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        int statusCode = response.statusCode();
+        String responseBody = response.body();
+        if (statusCode == 200) {
+            System.out.println("Abrigo cadastrado com sucesso!");
+            System.out.println(responseBody);
+        } else if (statusCode == 400 || statusCode == 500) {
+            System.out.println("Erro ao cadastrar o abrigo:");
+            System.out.println(responseBody);
         }
     }
 }
